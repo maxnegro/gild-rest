f236b75645424ab812093a27c2fb3b04cbc88dd0
Massimiliano Masserelli <massimiliano.masserelli@essepi.eu>
2018-01-03 18:32:26 +0100

    Modifica layout front page

--- front-page.php
+++ front-page.php
@@ -2,17 +2,24 @@
 
 <main class="main" id="js-main-page-single">
 
-	<?php while ( have_posts() ) : the_post(); ?>
-
-		<?php get_template_part('template-parts/page/content'); ?>
-
-    <?php
-      if ( comments_open() || get_comments_number() ) :
-        comments_template();
-      endif;
-    ?>
-
-  <?php endwhile; ?>
+  <section class="section">
+    <div class="section-inner">
+      <div class="section-title" id="js-section-title">
+        <h2><span><?php esc_html_e( 'Archives', 'gildrest' ); ?></span></h2>
+      </div>
+      <div class="section-content">
+        <?php if ( have_posts() ) : ?>
+          <div class="articles">
+            <?php while ( have_posts() ) : the_post(); ?>
+              <?php get_template_part('template-parts/page/content'); ?>
+            <?php endwhile; ?>
+          </div>
+        <?php else : ?>
+            <?php get_template_part('template-parts/common/articles-none'); ?>
+        <?php endif; ?>
+      </div>
+    </div>
+  </section>
 
 </main>
 
